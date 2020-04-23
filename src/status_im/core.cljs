bdb1a3ca3502e04033fbf5c63eac7fb0dbdac625
yenda <eric@status.im>
2020-04-23 12:13:49 +0200

    mvp

--- src/status_im/core.cljs
+++ src/status_im/core.cljs
@@ -1,11 +1,11 @@
 (ns status-im.core
-  (:require [re-frame.core :as re-frame]
-            [status-im.utils.error-handler :as error-handler]
-            [status-im.utils.platform :as platform]
+  (:require #_[re-frame.core :as re-frame]
+            #_[status-im.utils.error-handler :as error-handler]
+            #_[status-im.utils.platform :as platform]
             [status-im.ui.components.react :as react]
             [reagent.core :as reagent]
-            [status-im.utils.logging.core :as utils.logs]
-            cljs.core.specs.alpha
+            #_[status-im.utils.logging.core :as utils.logs]
+            #_cljs.core.specs.alpha
             ["react-native" :as react-native]))
 
 (if js/goog.DEBUG
@@ -15,8 +15,14 @@
                         "Warning: componentWillUpdate has been renamed, and is not recommended for use. See https://fb.me/react-async-component-lifecycle-hooks for details."])
   (aset js/console "disableYellowBox" true))
 
-(defn init [app-root]
+(defn app-root []
+  [react/view {}
+   [react/text "hello"]])
+
+(defn init []
   #_(utils.logs/init-logs)
   #_(error-handler/register-exception-handler!)
   #_(re-frame/dispatch-sync [:init/app-started])
-  (.registerComponent react/app-registry "StatusIm" #(reagent/reactify-component app-root)))
+  (enableScreens)
+  (.registerComponent react/app-registry "StatusIm" #(reagent/reactify-component app-root))
+  (.hide ^js react/splash-screen))

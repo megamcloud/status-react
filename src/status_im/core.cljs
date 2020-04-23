c7c8dd86e37ef6d90b9c18ebf11b6cfec1da9143
yenda <eric@status.im>
2020-04-23 14:08:58 +0200

    add react

--- src/status_im/core.cljs
+++ src/status_im/core.cljs
@@ -2,13 +2,12 @@
   (:require [re-frame.core :as re-frame]
             [status-im.utils.error-handler :as error-handler]
             #_[status-im.utils.platform :as platform]
-            #_[status-im.ui.components.react :as react]
+            [status-im.ui.components.react :as react]
             [reagent.core :as reagent]
             ["react-native-screens" :refer (enableScreens)]
             [status-im.utils.logging.core :as utils.logs]
             #_cljs.core.specs.alpha
             ["react-native" :as rn]))
-
 
 (if js/goog.DEBUG
   (.ignoreWarnings (.-YellowBox ^js rn)
@@ -18,32 +17,11 @@
   (aset js/console "disableYellowBox" true))
 
 (def app-registry (.-AppRegistry rn))
-
-(def view (reagent/adapt-react-class (.-View rn)))
-
-(def ^:private text-class (reagent/adapt-react-class (.-Text rn)))
-
 (def splash-screen (-> rn .-NativeModules .-SplashScreen))
 
-(defn- default-text-style-props [styles]
-  (merge {:font-family "inherit"}
-         styles))
-
-(defn- prepare-text-props [props]
-  (-> props
-      (update :style default-text-style-props)))
-
-(defn text
-  ([text-element]
-   (text {} text-element))
-  ([options text-element]
-   [text-class (prepare-text-props options) text-element])
-  ([options text-element nested-text]
-   [text-class (prepare-text-props options) text-element nested-text]))
-
 (defn app-root []
-  [view {}
-   [text "hello"]])
+  [react/view {}
+   [react/text "hello"]])
 
 (defn init []
   (utils.logs/init-logs)
--- src/status_im/ui/components/react.cljs
+++ src/status_im/ui/components/react.cljs
@@ -1,28 +1,24 @@
 (ns status-im.ui.components.react
-  (:require-macros [status-im.utils.views :as views])
-  (:require [goog.object :as object]
-            [reagent.core :as reagent]
+  #_(:require-macros [status-im.utils.views :as views])
+  (:require [reagent.core :as reagent]
             [status-im.ui.components.styles :as styles]
             [status-im.utils.utils :as utils]
             [status-im.utils.core :as utils.core]
             [status-im.utils.platform :as platform]
             #_[status-im.i18n :as i18n]
             ["react-native" :as react-native :refer (Keyboard)]
-            ["react-native-image-crop-picker" :default image-picker]
-            ["react-native-gesture-handler" :refer (TouchableWithoutFeedback)]
-            ["react-native-safe-area-context" :as safe-area-context
-             :refer (SafeAreaView SafeAreaProvider SafeAreaConsumer)]
-            [status-im.ui.components.colors :as colors]
+            #_["react-native-image-crop-picker" :default image-picker]
+            #_["react-native-gesture-handler" :refer (TouchableWithoutFeedback)]
+            #_["react-native-safe-area-context" :as safe-area-context
+               :refer (SafeAreaView SafeAreaProvider SafeAreaConsumer)]
+            #_[status-im.ui.components.colors :as colors]
             [status-im.ui.components.typography :as typography]))
 
 (def native-modules (.-NativeModules react-native))
 (def device-event-emitter (.-DeviceEventEmitter react-native))
 
-(def splash-screen (.-SplashScreen native-modules))
-
 ;; React Components
 
-(def app-registry (.-AppRegistry react-native))
 (def app-state (.-AppState react-native))
 (def view (reagent/adapt-react-class (.-View react-native)))
 (def progress-bar (reagent/adapt-react-class (.-ProgressBarAndroid react-native)))
@@ -38,53 +34,53 @@
 (def picker-class (reagent/adapt-react-class (.-Picker react-native)))
 (def picker-item-class (reagent/adapt-react-class (.-Item (.-Picker react-native))))
 
-(defn valid-source? [source]
-  (or (not (map? source))
-      (not (contains? source :uri))
-      (and (contains? source :uri)
-           (:uri source))))
+;; (defn valid-source? [source]
+;;   (or (not (map? source))
+;;       (not (contains? source :uri))
+;;       (and (contains? source :uri)
+;;            (:uri source))))
 
-(defn image [{:keys [source] :as props}]
-  (when (valid-source? source)
-    [image-class props]))
+;; (defn image [{:keys [source] :as props}]
+;;   (when (valid-source? source)
+;;     [image-class props]))
 
-(def switch-class (reagent/adapt-react-class (.-Switch react-native)))
+;; (def switch-class (reagent/adapt-react-class (.-Switch react-native)))
 
-(defn switch [props]
-  [switch-class props])
+;; (defn switch [props]
+;;   [switch-class props])
 
-(def touchable-highlight-class (reagent/adapt-react-class (.-TouchableHighlight react-native)))
-(def touchable-without-feedback-class (reagent/adapt-react-class (.-TouchableWithoutFeedback react-native)))
-(def touchable-opacity-class (reagent/adapt-react-class (.-TouchableOpacity react-native)))
-(def activity-indicator-class (reagent/adapt-react-class (.-ActivityIndicator react-native)))
+;; (def touchable-highlight-class (reagent/adapt-react-class (.-TouchableHighlight react-native)))
+;; (def touchable-without-feedback-class (reagent/adapt-react-class (.-TouchableWithoutFeedback react-native)))
+;; (def touchable-opacity-class (reagent/adapt-react-class (.-TouchableOpacity react-native)))
+;; (def activity-indicator-class (reagent/adapt-react-class (.-ActivityIndicator react-native)))
 
-(defn activity-indicator [props]
-  [activity-indicator-class props])
+;; (defn activity-indicator [props]
+;;   [activity-indicator-class props])
 
-(defn small-loading-indicator [color]
-  [activity-indicator {:color   (if color color :colors/gray)
-                       :ios     {:size :small}
-                       :android {:size :16}}])
+;; (defn small-loading-indicator [color]
+;;   [activity-indicator {:color   (if color color :colors/gray)
+;;                        :ios     {:size :small}
+;;                        :android {:size :16}}])
 
-(def modal (reagent/adapt-react-class (.-Modal react-native)))
+;; (def modal (reagent/adapt-react-class (.-Modal react-native)))
 
-(def pan-responder (.-PanResponder react-native))
-(def animated (.-Animated react-native))
+;; (def pan-responder (.-PanResponder react-native))
+;; (def animated (.-Animated react-native))
 
-(def animated-view-class
-  (reagent/adapt-react-class (.-View animated)))
+;; (def animated-view-class
+;;   (reagent/adapt-react-class (.-View animated)))
 
-(def animated-flat-list-class
-  (reagent/adapt-react-class (.-FlatList animated)))
+;; (def animated-flat-list-class
+;;   (reagent/adapt-react-class (.-FlatList animated)))
 
-(defn animated-view [props & content]
-  (vec (conj content props animated-view-class)))
+;; (defn animated-view [props & content]
+;;   (vec (conj content props animated-view-class)))
 
-(def dimensions (.-Dimensions react-native))
-(def keyboard (.-Keyboard react-native))
-(def dismiss-keyboard! #(.dismiss Keyboard))
-(def linking (.-Linking react-native))
-(def desktop-notification (.-DesktopNotification (.-NativeModules react-native)))
+;; (def dimensions (.-Dimensions react-native))
+;; (def keyboard (.-Keyboard react-native))
+;; (def dismiss-keyboard! #(.dismiss Keyboard))
+;; (def linking (.-Linking react-native))
+;; (def desktop-notification (.-DesktopNotification (.-NativeModules react-native)))
 
 (def max-font-size-multiplier 1.25)
 
@@ -93,11 +89,11 @@
       (update :style typography/get-style)
       (assoc :max-font-size-multiplier max-font-size-multiplier)))
 
-(defn prepare-nested-text-props [props]
-  (-> props
-      (update :style typography/get-nested-style)
-      (assoc :parseBasicMarkdown true)
-      (assoc :nested? true)))
+;; (defn prepare-nested-text-props [props]
+;;   (-> props
+;;       (update :style typography/get-nested-style)
+;;       (assoc :parseBasicMarkdown true)
+;;       (assoc :nested? true)))
 
 ;; Accessor methods for React Components
 (defn text
@@ -107,162 +103,162 @@
   ([options text-element]
    [text-class (prepare-text-props options) text-element]))
 
-(defn nested-text
-  "Returns nested text elements with proper styling and typography
-  Do not use the nested? option, it is for internal usage of the function only"
-  [options & nested-text-elements]
-  (let [options-with-style (if (:nested? options)
-                             (prepare-nested-text-props options)
-                             (prepare-text-props options))]
-    (reduce (fn [acc text-element]
-              (conj acc
-                    (if (string? text-element)
-                      text-element
-                      (let [[options & nested-text-elements] text-element]
-                        (apply nested-text (prepare-nested-text-props options)
-                               nested-text-elements)))))
-            [text-class (dissoc options-with-style :nested?)]
-            nested-text-elements)))
+;; (defn nested-text
+;;   "Returns nested text elements with proper styling and typography
+;;   Do not use the nested? option, it is for internal usage of the function only"
+;;   [options & nested-text-elements]
+;;   (let [options-with-style (if (:nested? options)
+;;                              (prepare-nested-text-props options)
+;;                              (prepare-text-props options))]
+;;     (reduce (fn [acc text-element]
+;;               (conj acc
+;;                     (if (string? text-element)
+;;                       text-element
+;;                       (let [[options & nested-text-elements] text-element]
+;;                         (apply nested-text (prepare-nested-text-props options)
+;;                                nested-text-elements)))))
+;;             [text-class (dissoc options-with-style :nested?)]
+;;             nested-text-elements)))
 
-;; We track all currently mounted text input refs
-;; in a ref-to-defaultValue map
-;; so that we can clear them (restore their default values)
-;; when global react-navigation's onWillBlur event is invoked
-(def text-input-refs (atom {}))
+;; ;; We track all currently mounted text input refs
+;; ;; in a ref-to-defaultValue map
+;; ;; so that we can clear them (restore their default values)
+;; ;; when global react-navigation's onWillBlur event is invoked
+;; (def text-input-refs (atom {}))
 
-(defn text-input
-  [options t #_text]
-  [text "hello"]
-  #_(let [render-fn (fn [options text]
-                      [text-input-class
-                       (merge
-                        {:underline-color-android  :transparent
-                         :max-font-size-multiplier max-font-size-multiplier
-                         :placeholder-text-color   colors/text-gray
-                         :placeholder              (i18n/label :t/type-a-message)
-                         :value                    text}
-                        (-> options
-                            (dissoc :preserve-input?)
-                            (update :style typography/get-style)
-                            (update :style dissoc :line-height)))])]
-      (if (:preserve-input? options)
-        render-fn
-        (let [input-ref (atom nil)]
-          (reagent/create-class
-           {:component-will-unmount #(when @input-ref
-                                       (swap! text-input-refs dissoc @input-ref))
-            :reagent-render
-            (fn [options text]
-              (render-fn (assoc options :ref
-                                (fn [r]
-                                  ;; Store input and its defaultValue
-                                  ;; one we receive a non-nil ref
-                                  (when (and r (nil? @input-ref))
-                                    (swap! text-input-refs assoc r (:default-value options)))
-                                  (reset! input-ref r)
-                                  (when (:ref options)
-                                    ((:ref options) r)))) text))})))))
+;; (defn text-input
+;;   [options t #_text]
+;;   [text "hello"]
+;;   #_(let [render-fn (fn [options text]
+;;                       [text-input-class
+;;                        (merge
+;;                         {:underline-color-android  :transparent
+;;                          :max-font-size-multiplier max-font-size-multiplier
+;;                          :placeholder-text-color   colors/text-gray
+;;                          :placeholder              (i18n/label :t/type-a-message)
+;;                          :value                    text}
+;;                         (-> options
+;;                             (dissoc :preserve-input?)
+;;                             (update :style typography/get-style)
+;;                             (update :style dissoc :line-height)))])]
+;;       (if (:preserve-input? options)
+;;         render-fn
+;;         (let [input-ref (atom nil)]
+;;           (reagent/create-class
+;;            {:component-will-unmount #(when @input-ref
+;;                                        (swap! text-input-refs dissoc @input-ref))
+;;             :reagent-render
+;;             (fn [options text]
+;;               (render-fn (assoc options :ref
+;;                                 (fn [r]
+;;                                   ;; Store input and its defaultValue
+;;                                   ;; one we receive a non-nil ref
+;;                                   (when (and r (nil? @input-ref))
+;;                                     (swap! text-input-refs assoc r (:default-value options)))
+;;                                   (reset! input-ref r)
+;;                                   (when (:ref options)
+;;                                     ((:ref options) r)))) text))})))))
 
-(defn i18n-text
-  [{:keys [style key]}]
-  [text {} "hello"]
-  #_[text {:style  style} (i18n/label key)])
+;; (defn i18n-text
+;;   [{:keys [style key]}]
+;;   [text {} "hello"]
+;;   #_[text {:style  style} (i18n/label key)])
 
-(defn icon
-  ([n] (icon n {:width  24
-                :height 24}))
-  ([n style]
-   [image {:source     {:uri (keyword (str "icon_" (name n)))}
-           :resizeMode "contain"
-           :style      style}]))
+;; (defn icon
+;;   ([n] (icon n {:width  24
+;;                 :height 24}))
+;;   ([n style]
+;;    [image {:source     {:uri (keyword (str "icon_" (name n)))}
+;;            :resizeMode "contain"
+;;            :style      style}]))
 
-(defn touchable-opacity [props content]
-  [touchable-opacity-class props content])
+;; (defn touchable-opacity [props content]
+;;   [touchable-opacity-class props content])
 
-(defn touchable-highlight [props content]
-  [touchable-highlight-class
-   (merge {:underlay-color :transparent} props)
-   content])
+;; (defn touchable-highlight [props content]
+;;   [touchable-highlight-class
+;;    (merge {:underlay-color :transparent} props)
+;;    content])
 
-(defn touchable-without-feedback [props content]
-  [touchable-without-feedback-class
-   props
-   content])
+;; (defn touchable-without-feedback [props content]
+;;   [touchable-without-feedback-class
+;;    props
+;;    content])
 
-(defn get-dimensions [name]
-  (js->clj (.get dimensions name) :keywordize-keys true))
+;; (defn get-dimensions [name]
+;;   (js->clj (.get dimensions name) :keywordize-keys true))
 
-(defn list-item [component]
-  (reagent/as-element component))
+;; (defn list-item [component]
+;;   (reagent/as-element component))
 
-(defn value->picker-item [{:keys [value label]}]
-  [picker-item-class {:value (or value "") :label (or label value "")}])
+;; (defn value->picker-item [{:keys [value label]}]
+;;   [picker-item-class {:value (or value "") :label (or label value "")}])
 
-(defn picker [{:keys [style on-change selected enabled data]}]
-  (into
-   [picker-class (merge (when style {:style style})
-                        (when enabled {:enabled enabled})
-                        (when on-change {:on-value-change on-change})
-                        (when selected {:selected-value selected}))]
-   (map value->picker-item data)))
+;; (defn picker [{:keys [style on-change selected enabled data]}]
+;;   (into
+;;    [picker-class (merge (when style {:style style})
+;;                         (when enabled {:enabled enabled})
+;;                         (when on-change {:on-value-change on-change})
+;;                         (when selected {:selected-value selected}))]
+;;    (map value->picker-item data)))
 
-;; Image picker
-(defn show-access-error [o]
-  #_(when (= "E_PERMISSION_MISSING" (.-code ^js o))
-      (utils/show-popup (i18n/label :t/error)
-                        (i18n/label :t/photos-access-error))))
+;; ;; Image picker
+;; (defn show-access-error [o]
+;;   #_(when (= "E_PERMISSION_MISSING" (.-code ^js o))
+;;       (utils/show-popup (i18n/label :t/error)
+;;                         (i18n/label :t/photos-access-error))))
 
-(defn show-image-picker
-  ([images-fn]
-   (show-image-picker images-fn nil))
-  ([images-fn media-type]
-   (-> image-picker
-       (.openPicker (clj->js {:multiple false :mediaType (or media-type "any")}))
-       (.then images-fn)
-       (.catch show-access-error))))
+;; (defn show-image-picker
+;;   ([images-fn]
+;;    (show-image-picker images-fn nil))
+;;   ([images-fn media-type]
+;;    (-> image-picker
+;;        (.openPicker (clj->js {:multiple false :mediaType (or media-type "any")}))
+;;        (.then images-fn)
+;;        (.catch show-access-error))))
 
-;; Clipboard
+;; ;; Clipboard
 
-(def sharing
-  (.-Share react-native))
+;; (def sharing
+;;   (.-Share react-native))
 
-(defn copy-to-clipboard [text]
-  (.setString (.-Clipboard react-native) text))
+;; (defn copy-to-clipboard [text]
+;;   (.setString (.-Clipboard react-native) text))
 
-(defn get-from-clipboard [clbk]
-  (let [clipboard-contents (.getString (.-Clipboard react-native))]
-    (.then clipboard-contents #(clbk %))))
+;; (defn get-from-clipboard [clbk]
+;;   (let [clipboard-contents (.getString (.-Clipboard react-native))]
+;;     (.then clipboard-contents #(clbk %))))
 
-;; KeyboardAvoidingView
+;; ;; KeyboardAvoidingView
 
-(defn keyboard-avoiding-view [props & children]
-  (into [keyboard-avoiding-view-class
-         (merge (when platform/ios? {:behavior :padding}) props)]
-        children))
+;; (defn keyboard-avoiding-view [props & children]
+;;   (into [keyboard-avoiding-view-class
+;;          (merge (when platform/ios? {:behavior :padding}) props)]
+;;         children))
 
-(defn scroll-view [props & children]
-  (vec (conj children props scroll-view-class)))
+;; (defn scroll-view [props & children]
+;;   (vec (conj children props scroll-view-class)))
 
-(views/defview with-activity-indicator
-  [{:keys [timeout style enabled? preview]} comp]
-  (views/letsubs
-    [loading (reagent/atom true)]
-    {:component-did-mount (fn []
-                            (if (or (nil? timeout)
-                                    (> 100 timeout))
-                              (reset! loading false)
-                              (utils/set-timeout #(reset! loading false)
-                                                 timeout)))}
-    (if (and (not enabled?) @loading)
-      (or preview
-          [view {:style (or style {:justify-content :center
-                                   :align-items     :center})}
-           [activity-indicator {:animating true}]])
-      comp)))
+;; (views/defview with-activity-indicator
+;;   [{:keys [timeout style enabled? preview]} comp]
+;;   (views/letsubs
+;;     [loading (reagent/atom true)]
+;;     {:component-did-mount (fn []
+;;                             (if (or (nil? timeout)
+;;                                     (> 100 timeout))
+;;                               (reset! loading false)
+;;                               (utils/set-timeout #(reset! loading false)
+;;                                                  timeout)))}
+;;     (if (and (not enabled?) @loading)
+;;       (or preview
+;;           [view {:style (or style {:justify-content :center
+;;                                    :align-items     :center})}
+;;            [activity-indicator {:animating true}]])
+;;       comp)))
 
-(def safe-area-provider (reagent/adapt-react-class SafeAreaProvider))
-(def safe-area-consumer (reagent/adapt-react-class SafeAreaConsumer))
+;; (def safe-area-provider (reagent/adapt-react-class SafeAreaProvider))
+;; (def safe-area-consumer (reagent/adapt-react-class SafeAreaConsumer))
 
-(def safe-area-view (reagent/adapt-react-class SafeAreaView))
+;; (def safe-area-view (reagent/adapt-react-class SafeAreaView))
 
-(def touchable-without-feedback-gesture (reagent/adapt-react-class TouchableWithoutFeedback))
+;; (def touchable-without-feedback-gesture (reagent/adapt-react-class TouchableWithoutFeedback))

5f06f2ef133c6fc19c8b1df6420346ccb95fdb09
yenda <eric@status.im>
2020-04-23 17:36:51 +0200

    d

--- src/status_im/core.cljs
+++ src/status_im/core.cljs
@@ -20,8 +20,37 @@
 (def app-registry (.-AppRegistry rn))
 (def splash-screen (-> rn .-NativeModules .-SplashScreen))
 
-(defn app-root []
-  [views/main])
+(defn app-state-change-handler [state]
+  (re-frame/dispatch [:app-state-change state]))
+
+(defn app-root [props]
+  (let [keyboard-height (re-frame/subscribe [:keyboard-height])]
+    (reagent/create-class
+     {:component-did-mount
+      (fn [this]
+        #_(.addListener react/keyboard
+                        "keyboardWillShow"
+                        (fn [^js e]
+                          (let [h (.. e -endCoordinates -height)]
+                            (when-not (= h @keyboard-height)
+                              (dispatch [:set :keyboard-height h])
+                              (dispatch [:set :keyboard-max-height h])))))
+        #_(.addListener react/keyboard
+                        "keyboardWillHide"
+                        #(when-not (= 0 @keyboard-height)
+                           (dispatch [:set :keyboard-height 0])))
+        (.addEventListener react/app-state "change" app-state-change-handler)
+        #_(.addEventListener react-native-languages "change" on-languages-change)
+        #_(.addEventListener react-native-shake
+                             "ShakeEvent"
+                             on-shake)
+        (re-frame/dispatch [:set-initial-props (reagent/props this)]))
+      :component-will-unmount
+      (fn []
+        #_(.removeEventListener react/app-state "change" app-state-change-handler)
+        #_(.removeEventListener react-native-languages "change" on-languages-change))
+      :display-name "root"
+      :reagent-render views/main})))
 
 (defn init []
   (utils.logs/init-logs)
